69bed43bbca0536a2450a75983eb78f0d4a993da
DESKTOP-M3DS20J\burak <burakgumusfb@gmail.com>
2018-10-08 21:44:42 +0300

    butterknife

--- app/src/main/java/com/czh/testmpeg/HomeActivity.java
+++ app/src/main/java/com/czh/testmpeg/HomeActivity.java
@@ -2,18 +2,31 @@ package com.czh.testmpeg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.czh.testmpeg.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class HomeActivity extends BaseActivity {
 
+    @BindView(R.id.firstBtn)
+    Button first_Button;
+    @OnClick(R.id.firstBtn) void Selam(){
+        showToastMethod("Selam");
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResourceId());
+
         setDefaults("key1", "selam", getApplicationContext());
-        Intent homepage = new Intent(HomeActivity.this, ShowActivity.class);
-        startActivity(homepage);
+       // Intent homepage = new Intent(HomeActivity.this, ShowActivity.class);
+        //startActivity(homepage);
+
+        ButterKnife.bind(this);
     }
 
     @Override
